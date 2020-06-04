6252f4c14237854448a8922daa92312126cf4f96
Paul Martin <paul.y.martin@johnlewis.co.uk>
2020-06-04 22:01:57 +0100

    3.12 implement folds with the opposite-handed fold
    i.e. foldLeft using foldRight, and foldRight using foldLeft.
    
    I did both of these by first reversing the list with a fold, and then
    doing the opposite-handed fold on the reversed list.
    
    Reversing is easy with foldLeft, gnarly with foldRight. Uses a recursive
    function that will be very slow. Doubt this is the solution the book had
    in mind.

--- src/main/kotlin/chapter3/SinglyLinkedList.kt
+++ src/main/kotlin/chapter3/SinglyLinkedList.kt
@@ -24,6 +24,22 @@ sealed class List<out A> {
                 is Nil -> acc
                 is Cons -> foldLeft(xs.tail, f(acc, xs.head), f)
             }
+
+        fun <A, B> foldRightL(xs: List<A>, acc: B, f: (A, B) -> B): B {
+            val reversedXs = foldLeft(xs, empty<A>()) { z, a -> Cons(a, z) }
+            return foldLeft(reversedXs, acc) { b, a -> f(a, b) }
+        }
+
+        fun <A, B> foldLeftR(xs: List<A>, acc: B, f: (B, A) -> B): B {
+            fun g(a: A, b: List<A>): List<A> =
+                when (b) {
+                    is Nil -> Cons(a, b)
+                    else -> Cons(b.head(), g(a, b.tail()))
+                }
+
+            val reversedXs = foldRight(xs, empty<A>()) { a, acc -> g(a, acc) }
+            return foldRight(reversedXs, acc) { a, b -> f(b, a) }
+        }
     }
 }
 
@@ -34,6 +50,11 @@ data class Cons<out A>(val head: A, val tail: List<A>) : List<A>()
 fun <A> List<A>.tail(): List<A> {
     require(this is Cons) { "Can't get tail of Nil" }
     return tail
+}
+
+fun <A> List<A>.head(): A {
+    require(this is Cons) { "Can't get head of Nil" }
+    return head
 }
 
 fun <A> List<A>.setHead(x: A): List<A> {
@@ -74,7 +95,16 @@ fun <A> List<A>.init(): List<A> {
 
 fun <A> List<A>.length(): Int = List.foldRight(this, 0) { _, acc -> acc + 1 }
 
-fun <A> List<A>.reverse(): List<A> = List.foldLeft(this, List.empty<A>()) { acc, a -> Cons(a, acc)}
+fun <A> List<A>.reverseL(): List<A> = List.foldLeft(this, List.empty<A>()) { acc, a -> Cons(a, acc) }
+
+fun <A> List<A>.reverseR(): List<A> {
+    fun f(a: A, b: List<A>): List<A> =
+        when (b) {
+            is Nil -> Cons(a, b)
+            else -> Cons(b.head(), f(a, b.tail()))
+        }
+    return List.foldRight(this, List.empty<A>()) { a, acc -> f(a, acc) }
+}
 
 fun main() {
     val a = List.of(1, 2, 3, 4)
@@ -83,6 +113,11 @@ fun main() {
     println(List.sum(a))
     println(List.product(b))
 
-    println(a.reverse())
-    println(b.reverse())
+    println(a.reverseL())
+    println(a.reverseR())
+
+    println(List.foldRight(a, List.empty<Int>()) { a, b -> Cons(a, b) })
+    println(List.foldRightL(a, List.empty<Int>()) { a, b -> Cons(a, b) })
+    println(List.foldLeft(a, List.empty<Int>()) { a, b -> Cons(b, a) })
+    println(List.foldLeftR(a, List.empty<Int>()) { a, b -> Cons(b, a) })
 }
