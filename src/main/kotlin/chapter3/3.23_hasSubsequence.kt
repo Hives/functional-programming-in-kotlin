5c923c482e4f64e04ddd59950766ec1e00fd5a4e
Paul Martin <paul.y.martin@johnlewis.co.uk>
2020-06-15 21:57:10 +0100

    3.23 hasSubsequence improved
    Using `startsWith` was a suggestion from the book

--- src/main/kotlin/chapter3/3.23_hasSubsequence.kt
+++ src/main/kotlin/chapter3/3.23_hasSubsequence.kt
@@ -1,11 +1,16 @@
 package chapter3
 
-fun <A> List<A>.hasSubsequence(xs: List<A>): Boolean =
+tailrec fun <A> List<A>.startsWith(xs: List<A>): Boolean =
     if (xs == Nil) true
     else if (this == Nil) false
-    else if (this.head() == xs.head()) this.tail().hasSubsequence(xs.tail()) || this.tail().hasSubsequence(xs)
+    else if (this.head() == xs.head()) this.tail().startsWith(xs.tail())
+    else false
+
+tailrec fun <A> List<A>.hasSubsequence(xs: List<A>): Boolean =
+    if (xs == Nil) true
+    else if (this.startsWith(xs)) true
     else this.tail().hasSubsequence(xs)
 
 fun main() {
-    println(List.of(0, 1, 2, 3).hasSubsequence(List.of(1, 2, 3)))
+    println(List.of(0, 1, 2, 3).hasSubsequence(List.of(1)))
 }
