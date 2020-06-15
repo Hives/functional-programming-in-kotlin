ed0fc9dd86ca71e82241b6f27c474b223186b8a6
Paul Martin <paul.y.martin@johnlewis.co.uk>
2020-06-15 21:30:10 +0100

    3.22-23 - zipping

---
+++ src/main/kotlin/chapter3/3.22_zip.kt
@@ -0,0 +1,9 @@
+package chapter3
+
+fun <X, Y, Z> zipWith(xs: List<X>, ys: List<Y>, f: (X, Y) -> Z): List<Z> =
+    if (xs == Nil || ys == Nil) Nil
+    else Cons(f(xs.head(), ys.head()), zipWith(xs.tail(), ys.tail(), f))
+
+fun main() {
+    println(zipWith(List.of(0, 1, 2, 3), List.of(4, 5, 6, 7)) { x, y -> x * y })
+}
