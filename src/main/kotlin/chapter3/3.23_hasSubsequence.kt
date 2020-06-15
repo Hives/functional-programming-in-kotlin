694eb9351818e491959ee85901fe9e82a45c0c4d
Paul Martin <paul.y.martin@johnlewis.co.uk>
2020-06-16 00:31:28 +0100

    3.23 hasSubsequence improvement

--- src/main/kotlin/chapter3/3.23_hasSubsequence.kt
+++ src/main/kotlin/chapter3/3.23_hasSubsequence.kt
@@ -1,16 +1,36 @@
 package chapter3
 
 tailrec fun <A> List<A>.startsWith(xs: List<A>): Boolean =
-    if (xs == Nil) true
-    else if (this == Nil) false
-    else if (this.head() == xs.head()) this.tail().startsWith(xs.tail())
-    else false
+    when (xs) {
+        is Nil -> true
+        is Cons -> when (this) {
+            is Nil -> false
+            is Cons ->
+                if (this.head() == xs.head()) this.tail().startsWith(xs.tail())
+                else false
+        }
+    }
 
 tailrec fun <A> List<A>.hasSubsequence(xs: List<A>): Boolean =
-    if (xs == Nil) true
-    else if (this.startsWith(xs)) true
-    else this.tail().hasSubsequence(xs)
+    when (this) {
+        is Nil -> false
+        is Cons ->
+            if (this.startsWith(xs)) true
+            else this.tail().hasSubsequence(xs)
+    }
 
 fun main() {
+    println(List.empty<Int>().startsWith(List.empty()))
+    println(List.empty<Int>().startsWith(List.of(1)))
+    println(List.of(0, 1, 2, 3).startsWith(List.empty()))
+    println(List.of(0, 1, 2, 3).startsWith(List.of(0)))
+    println(List.of(0, 1, 2, 3).startsWith(List.of(0, 1)))
+    println(List.of(0, 1, 2, 3).startsWith(List.of(1)))
+
     println(List.of(0, 1, 2, 3).hasSubsequence(List.of(1)))
+    println(List.of(0, 1, 2, 3).hasSubsequence(List.of(1, 2)))
+    println(List.of(0, 1, 2, 3).hasSubsequence(List.of(0, 1, 2)))
+    println(List.of(0, 1, 2, 3).hasSubsequence(List.of(0, 1, 2, 3)))
+    println(List.of(0, 1, 2, 3).hasSubsequence(List.of(1, 2, 4)))
+    println(List.empty<Int>().hasSubsequence(List.of(1)))
 }
