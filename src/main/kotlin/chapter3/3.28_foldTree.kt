a975fb559699dab2dbd9645a49e44903009c0341
Paul Martin <paul.y.martin@johnlewis.co.uk>
2020-06-16 01:21:28 +0100

    Refactor

--- src/main/kotlin/chapter3/3.28_foldTree.kt
+++ src/main/kotlin/chapter3/3.28_foldTree.kt
@@ -6,13 +6,14 @@ fun <A, B> fold(ta: Tree<A>, l: (A) -> B, b: (B, B) -> B): B =
         is Branch -> b(fold(ta.left, l, b), fold(ta.right, l, b))
     }
 
-fun <A> sizeF(ta: Tree<A>): Int = fold(ta, { 1 }) { b1, b2 -> 1 + b1 + b2 }
+fun <A> sizeF(ta: Tree<A>): Int = fold(ta, { 1 }, { b1, b2 -> 1 + b1 + b2 })
 
-fun maximumF(ta: Tree<Int>): Int = fold(ta, { it }) { b1, b2 -> maxOf(b1, b2) }
+fun maximumF(ta: Tree<Int>): Int = fold(ta, { it }, { b1, b2 -> maxOf(b1, b2) })
 
-fun <A> depthF(ta: Tree<A>): Int = fold(ta, { 0 }) { b1, b2 -> 1 + maxOf(b1, b2) }
+fun <A> depthF(ta: Tree<A>): Int = fold(ta, { 0 }, { b1, b2 -> 1 + maxOf(b1, b2) })
 
-fun <A, B> mapF(ta: Tree<A>, f: (A) -> B): Tree<B> = fold(ta, { Leaf(f(it)) as Tree<B> }) { b1, b2 -> Branch(b1, b2) }
+fun <A, B> mapF(ta: Tree<A>, f: (A) -> B): Tree<B> =
+    fold(ta, { Leaf(f(it)) }, { b1: Tree<B>, b2: Tree<B> -> Branch(b1, b2) })
 
 fun main() {
     println(sizeF(Leaf("foo")))
