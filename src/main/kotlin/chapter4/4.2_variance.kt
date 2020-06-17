6785a97823d2f5c6e97dfe78f041eab6b420d9ad
Paul Martin <paul.y.martin@johnlewis.co.uk>
2020-06-17 01:47:37 +0100

    4.2 variance

---
+++ src/main/kotlin/chapter4/4.2_variance.kt
@@ -0,0 +1,13 @@
+package chapter4
+
+import kotlin.math.pow
+
+fun mean(xs: List<Double>): Option<Double> =
+    if (xs.isEmpty()) None
+    else Some(xs.sum() / xs.size)
+
+fun variance(xs: List<Double>): Option<Double> =
+    mean(xs)
+        .flatMap { mean ->
+            mean(xs.map { x -> (x - mean).pow(2) })
+        }
