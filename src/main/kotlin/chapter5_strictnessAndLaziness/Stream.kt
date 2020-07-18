f4010423feaebde10700c3733376aa5633fdd0bc
Paul Martin <paul.y.martin@johnlewis.co.uk>
2020-07-18 22:33:50 +0100

    Add Stream class

--- src/main/kotlin/chapter5_strictnessAndLaziness/Stream.kt
+++ src/main/kotlin/chapter5_strictnessAndLaziness/Stream.kt
@@ -1,2 +1,24 @@
 package chapter5_strictnessAndLaziness
 
+sealed class Stream<out A> {
+    companion object {
+        fun <A> cons(hd: () -> A, tl: () -> Stream<A>): Stream<A> {
+            val head: A by lazy { hd() }
+            val tail: Stream<A> by lazy { tl() }
+            return Cons({ head }, { tail })
+        }
+
+        fun <A> empty(): Stream<A> = Empty
+
+        fun <A> of(vararg xs: A): Stream<A> =
+            if (xs.isEmpty()) empty()
+            else cons({ xs[0] }, { of(*xs.sliceArray(1 until xs.size)) })
+    }
+}
+
+data class Cons<out A>(
+    val h: () -> A,
+    val t: () -> Stream<A>
+) : Stream<A>()
+
+object Empty : Stream<Nothing>()
