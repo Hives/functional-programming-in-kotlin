0d976d4b130f8b688cd3705ed8cf66504c500c06
Paul Martin <paul.y.martin@johnlewis.co.uk>
2021-02-28 18:09:24 +0000

    Refactor unfold

--- src/main/kotlin/chapter5_strictnessAndLaziness/11_unfold.kt
+++ src/main/kotlin/chapter5_strictnessAndLaziness/11_unfold.kt
@@ -1,19 +1,16 @@
 package chapter5_strictnessAndLaziness
 
-import chapter4_handlingErrors.None
 import chapter4_handlingErrors.Option
 import chapter4_handlingErrors.Some
+import chapter4_handlingErrors.getOrElse
+import chapter4_handlingErrors.map
 
-fun <A, S> unfold(z: S, f: (S) -> Option<Pair<A, S>>): Stream<A> {
-    val foo = f(z)
-    return when (foo) {
-        None -> Stream.empty()
-        is Some -> {
-            val (bar, baz) = foo.get
-            Cons({ bar }, { unfold(baz, f) })
-        }
+fun <A, S> unfold(state: S, f: (S) -> Option<Pair<A, S>>): Stream<A> =
+    f(state).map { (value, newState) ->
+        Cons({ value }, { unfold(newState, f) })
+    }.getOrElse {
+        Stream.empty()
     }
-}
 
 fun main() {
     val fibsy = unfold(Pair(0, 1)) { (n, m) -> Some(Pair(n, Pair(m, m + n))) }
